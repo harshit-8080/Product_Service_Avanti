92c9e824da9930be1e1eacf63d120a0cd95e2027
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-01-01 02:55:14 +0530

    adding apis for products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,6 +1,90 @@
 const {ProductModel} = require("../models/index");
 
-exports.test = async ( req, res ) => {
+exports.addProduct = async ( req, res ) => {
 
-    
+    try {
+        
+        const product = {
+            name:req.body.name,
+            desc: req.body.desc,
+            category: req.body.category,
+            price: req.body.price,
+            available:req.body.available,
+            unit:req.body.unit,
+            brand:req.body.brand
+        }
+
+        const response = await ProductModel.create(product);
+        return res.json({
+            "success":true,
+            "response":response
+        })
+
+    } catch (error) {
+        
+        console.log(error);
+
+        return res.json({
+            "msg":'internal server error'
+        })
+    }
 }   
+
+exports.getAllProducts = async ( req, res ) => {
+
+    try {
+        
+        const response = await ProductModel.find();
+        return res.json({
+            "success":true,
+            "response":response
+        })
+
+    } catch (error) {
+        
+        console.log(error);
+
+        return res.json({
+            "msg":'internal server error'
+        })
+    }
+} 
+
+exports.getProductById = async ( req, res ) => {
+
+    try {
+        
+        const response = await ProductModel.findById(req.params.productId);
+        return res.json({
+            "success":true,
+            "response":response
+        })
+
+    } catch (error) {
+        
+        console.log(error);
+
+        return res.json({
+            "msg":'internal server error'
+        })
+    }
+} 
+
+exports.getProductByCategory = async ( req, res ) => {
+
+    try {
+        const response = await ProductModel.find({category:req.query.search});
+        return res.json({
+            "success":true,
+            "response":response
+        })
+
+    } catch (error) {
+        
+        console.log(error);
+
+        return res.json({
+            "msg":error
+        })
+    }
+} 
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,9 +4,14 @@ const ProductController = require("../controllers/product.controller");
 
 const ProductRouter = express.Router();
 
-ProductRouter.get("/",ProductController.test);
+// ProductRouter.get("/",ProductController.test);
 
+ProductRouter.post("/add", ProductController.addProduct);
 
+ProductRouter.get("/", ProductController.getAllProducts);
 
+ProductRouter.get("/search/:productId/", ProductController.getProductById);
+
+ProductRouter.get("/category", ProductController.getProductByCategory);
 
 module.exports = ProductRouter;
